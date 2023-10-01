6e1f068f34bf371be341aaa16749ddaa2b100403
Gaurav <gaurav.b21@iiits.in>
2023-10-01 19:04:36 +0530

    6th commit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -48,8 +48,7 @@ export const login = async (req, res) => {
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ msg: "Invalid credentials. " });
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-
+    const token = jwt.sign({ id: user._id },`${process.env.JWT_SECRET}`);
     delete user.password;
     res.status(200).json({ token, user });
   } catch (err) {
