eb708d683bfcdc1c25305a45637e31681862be2d
Gaurav <gaurav.b21@iiits.in>
2023-10-01 18:57:48 +0530

    5th commit

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ import { users, posts } from "./data/index.js";
 
 const __filename = fileURLToPath(import.meta.url); 
 const __dirname = path.dirname(__filename);
-require("dotenv").config();
+dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
